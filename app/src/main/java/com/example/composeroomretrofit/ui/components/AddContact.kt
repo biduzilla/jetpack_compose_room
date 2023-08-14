07003e131e232629381d26144ee16a56c215968c
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2023-08-14 09:20:13 -0300

    Contact Screen

---
+++ app/src/main/java/com/example/composeroomretrofit/ui/components/AddContact.kt
@@ -0,0 +1,15 @@
+package com.example.composeroomretrofit.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.composeroomretrofit.ContactEvent
+import com.example.composeroomretrofit.ui.ContactState
+
+@Composable
+fun AddContact(
+    state: ContactState,
+    onEvent: (ContactEvent) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+}
